5208721200a9a27ea35d9fd55cc3cf18cff5f9ec
Stefan Horst <stefan@ultrachaos.de>
2023-02-21 23:32:50 +0100

    skip deleted and exclude_backup users from restic

--- metadata.py
+++ metadata.py
@@ -1,3 +1,6 @@
+global metadata_reactor, node, DoNotRunAgain
+
+
 @metadata_reactor
 def add_restic_rules(metadata):
     if not node.has_bundle("restic"):
@@ -6,10 +9,12 @@ def add_restic_rules(metadata):
     add_folders = ['/root', ]
 
     for username, user_attrs in metadata.get('users').items():
-        add_folders += ["/home/{}".format(username), ]
+        if user_attrs.get('delete', False) or user_attrs.get('exclude_backup', False):
+            continue
+        add_folders += [user_attrs.get('home', f"/home/{username}"), ]
 
     return {
         'restic': {
-            'backup_folders':add_folders
+            'backup_folders': add_folders
         }
     }
